0c19edf0a9c93b95f2de004d8a96d301f982a059
24leo24 <42220816+24leo24@users.noreply.github.com>
2023-06-24 01:36:35 -0400

    esto 2

--- ventana1.py
+++ ventana1.py
@@ -9,7 +9,7 @@ class Frame(tk.Frame):
         
         
 
-   
+        #esto 2
         self.boton_nuevo = tk.Button(self, text="CREAR CONTENEDOR")
         self.boton_nuevo.config(width=28, font=('Arial', 12, 'bold'),fg = '#DAD5D6', bg='#3371FF', cursor='hand2', activebackground='#33ACFF', command=self.mostrar_vista2)
         self.boton_nuevo.grid(row= 2, column=2, padx=50, pady= 45)
